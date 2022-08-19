3944a398092fcaae7028473e3d02b52691146397
Abhulimen Enahoro <abhulimenenahoro@gmail.com>
2022-08-19 13:25:48 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
             />
           </div>
           <div className={styles.left}>
-            <p>Hi, my name is <span>Abhulimen Enahoro</span>- but you can call me Ena :).</p>
+            <p>Hi, my name is <span>Abhulimen Samson Enahoro</span>- but you can call me Ena :).</p>
             <p>I am a Full stack software developer from Lagos, Nigeria skilled in <span>React</span>, <span>Typescript</span> and the <span>Django REST framework</span>.</p>
             <p>I have always been passionate about health care as it was the reason I got into tech in the first place, my passion was spurred by the severe lack of 21st century healthcare solutions in my community.</p>
             <p>I spend my free time listening to afrobeats music, watching anime, working out and currently; reading about how convolutional neural networks can be used in malignant tumor detection.</p>
